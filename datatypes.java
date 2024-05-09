41fdc938b553c8b71fd9855e080b76886176c3c0
KPAryan <ceo.karyan@gmail.com>
2024-05-09 15:18:13 +0530

    Initial commit

---
+++ datatypes.java
@@ -0,0 +1,9 @@
+public class datatypes {
+    public static void main(String[] args) {
+       byte b=126;
+       System.out.println(b);
+       char c='A';
+       System.out.println(c);
+       
+    }
+}
---
+++ stack.java
@@ -0,0 +1,11 @@
+public class stack {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4};
+        int n=a.length+1;
+        System.out.println(n);
+        for(int i=0;i<=n;i++){
+
+        }
+    }
+}
+
